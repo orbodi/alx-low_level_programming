65a04af146719d4a0d32758995c1ad324d6947d0
Abdourakibou OURO-BODI <ourobodiabdourakibou@gmail.com>
2022-10-19 20:30:51 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -23,8 +23,9 @@ int print_sign(int n)
 		return (0);
 
 	} else if (n < 0)
-																				{
-																					_putchar('-');
-																				}
-																				return (-1);
+	{
+		_putchar('-');												
+	}
+
+	return (-1);
 }
